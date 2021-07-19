37c223e5a98b29453fc786f34d0b9046dee83c7c
Alexander Gherardi <alexander.2003@live.com>
2021-07-19 12:35:00 -0700

    Initial Commit

---
+++ Switch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Switch
+//
+//  Created by Alexander Gherardi on 7/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Switch/SwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchApp.swift
+//  Switch
+//
+//  Created by Alexander Gherardi on 7/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
